e584e3f6e13d47f58b29d72c0d6e7a1e916a2da5
WeiWei <yher25@gmail.com>
2023-10-18 13:58:03 +0800

    fixed error

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import { Environment, Center } from "@react-three/drei";
+import { Center } from "@react-three/drei";
 import Shirt from "./Shirt";
 import Backdrop from "./Backdrop";
 import CameraRig from "./CameraRig";
@@ -12,8 +12,7 @@ const CanvasModel = () => {
       gl={{ preserveDrawingBuffer: true }}
       className="w-full max-w-full h-full transition-all ease-in"
     >
-      <ambientLight intensity={0.1} />
-      <Environment preset="city" />
+      <ambientLight intensity={1} />
       <CameraRig>
         <Backdrop />
 
